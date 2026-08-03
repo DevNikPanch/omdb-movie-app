94854acae8fd73b70412493eac31d2cdb95e7ad6
DevNikPanch <sibiryakov_7@mail.ru>
2026-08-03 17:32:25 +0400

    Add scss files and create Header without functional logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <TheHeader />
 </template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="header">
+        <div class="header__container">
+            <h1 class="header__logo">Film<span>Hub</span></h1>
+
+            <form class="header__search">
+                <input type="text" placeholder="Поиск фильмов (например, Matrix)..." class="header__input" />
+                <button type="submit" class="header__button">
+                    Найти
+                </button>
+            </form>
+        </div>
+    </header>
+</template>
