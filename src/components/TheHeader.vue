cd62b1c1d6812c637a51f1b9946ba2d2297aa326
DevNikPanch <sibiryakov_7@mail.ru>
2026-08-03 20:52:43 +0400

    Create pinia store, types for movies and integrate the search logic with the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useMovieStore } from '../stores/useMovieStore';
+
+const movieStore = useMovieStore();
+
+const searchQueryText = ref<string>('');
+
+const handleSearch = () => {
+    movieStore.searchMovies(searchQueryText.value);
+}
+
 </script>
 
 <template>
@@ -6,8 +17,9 @@
         <div class="header__container">
             <h1 class="header__logo">Film<span>Hub</span></h1>
 
-            <form class="header__search">
-                <input type="text" placeholder="Поиск фильмов (например, Matrix)..." class="header__input" />
+            <form class="header__search" @submit.prevent="handleSearch">
+                <input v-model="searchQueryText" type="text" placeholder="Поиск фильмов (например, Matrix)..."
+                    class="header__input" />
                 <button type="submit" class="header__button">
                     Найти
                 </button>
---
+++ src/stores/useMovieStore.ts
@@ -0,0 +1,38 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Movie, SearchResponse } from '../types/movie';
+
+export const useMovieStore = defineStore('movie', () => {
+    const movies = ref<Movie[]>([]);
+    const isLoading = ref<boolean>(false);
+    const error = ref<string | null>(null);
+
+    async function searchMovies(query: string) {
+        if (!query.trim()) return;
+        error.value = null;
+        isLoading.value = true;
+        try {
+            const response = await fetch(
+                `https://www.omdbapi.com/?apikey=e6867c0a&s=${encodeURIComponent(query)}`,
+            );
+
+            if (!response.ok) throw new Error('Ошибка сети!');
+
+            const data: SearchResponse = await response.json();
+
+            if (data.Response === 'True' && data.Search) {
+                movies.value = data.Search;
+            } else {
+                movies.value = [];
+                error.value = data.Error || 'Фильмы не найдены';
+            }
+            console.log(data);
+        } catch (err) {
+            error.value = 'Не удалось загрузить данные. Проверьте соединение.';
+            console.error(err);
+        } finally {
+            isLoading.value = false;
+        }
+    }
+    return { movies, isLoading, error, searchMovies };
+});
