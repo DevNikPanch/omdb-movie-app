2c63c505665309f070c56ffc7d26aec9f7054609
DevNikPanch <sibiryakov_7@mail.ru>
2026-08-04 19:23:40 +0400

    Add styles for MovieList and MovieCard

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,18 +1,28 @@
-<script setup lang="ts">
-import { useMovieStore } from '../stores/useMovieStore';
+<script setup lang="ts">import {
+    useMovieStore
+}
+
+    from '../stores/useMovieStore';
 import MovieCard from './MovieCard.vue';
 
 const movieStore = useMovieStore();
 
 </script>
-
 <template>
     <div v-if="movieStore.isLoading" class="movie-loading">Loading films...</div>
     <div v-else-if="movieStore.error" class="movie-error">
-        <p>{{ movieStore.error }}</p>
+        <p> {
+            {
+            movieStore.error
+            }
+            }
+
+        </p>
     </div>
     <div v-else-if="movieStore.movies.length" class="movie-list">
-        <MovieCard v-for="movie in movieStore.movies" :key="movie.imdbID" :movie="movie" />
+        <div class="movie-list__container">
+            <MovieCard v-for="movie in movieStore.movies" :key="movie.imdbID" :movie="movie" />
+        </div>
     </div>
     <div v-else class="movie-empty">
         <p>Введите название фильма в поиске выше</p>
