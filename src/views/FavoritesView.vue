4a837eaa898cfc69ecc1fca7486f8473f1ab6cc2
DevNikPanch <sibiryakov_7@mail.ru>
2026-09-01 14:01:39 +0400

    Add Layout of the Layout of the Favorites block and adding styles

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import MovieCard from '../components/MovieCard.vue';
+import { useFavoritesStore } from '../stores/useFavoritesStore';
+
+const favoritesStore = useFavoritesStore();
 
 </script>
 
 <template>
-    Заглушка
+    <section class="favorites">
+        <div class="favorites__container">
+            <h1 class="favorites__title">Избранные фильмы</h1>
+            <div v-if="!favoritesStore.countFavoritesMovies" class="favorites__empty">
+                <p class="favorites__empty-text">Ваш список избранного пуст</p>
+                <RouterLink to="/" class="favorites__empty-link"> Найти фильмы </RouterLink>
+            </div>
+            <div v-else class="favorites__grid">
+                <MovieCard v-for="movie in favoritesStore.favoritesMovies" :key="movie.imdbID" :movie="movie" />
+            </div>
+        </div>
+    </section>
 </template>
